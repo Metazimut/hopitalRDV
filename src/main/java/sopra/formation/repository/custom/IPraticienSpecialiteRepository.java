d0638ad15b10010b56189f890e6a7ea970155585
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-12 11:46:39 +0200

    correction 1

---
+++ src/main/java/sopra/formation/repository/custom/IPraticienSpecialiteRepository.java
@@ -0,0 +1,9 @@
+package sopra.formation.repository.custom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.formation.model.PraticienSpecialite;
+
+public interface IPraticienSpecialiteRepository extends JpaRepository<PraticienSpecialite, Long>{
+
+}
