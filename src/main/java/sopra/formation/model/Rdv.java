e55ddc9ee32e7fc47667ed4a9c74ab2fe5163e7c
Florian F <favetta.florian@gmail.com>
2021-08-12 10:45:38 +0200

    creneaux

--- src/main/java/sopra/formation/model/Rdv.java
+++ src/main/java/sopra/formation/model/Rdv.java
@@ -1,8 +1,6 @@
 package sopra.formation.model;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
