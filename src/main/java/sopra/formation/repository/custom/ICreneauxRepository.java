a8bd76bb90c3b4a13de6f318cedeb1d3968042f4
Stan <stanislas@gmail.com>
2021-08-12 11:13:34 +0200

    creneaux

---
+++ src/main/java/sopra/formation/repository/custom/ICreneauxRepository.java
@@ -0,0 +1,10 @@
+package sopra.formation.repository.custom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.formation.model.Compte;
+import sopra.formation.model.Creneaux;
+
+public interface ICreneauxRepository extends JpaRepository<Creneaux, Long> {
+
+}
